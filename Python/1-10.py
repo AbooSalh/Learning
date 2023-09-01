bd717475a87649b751a248465a0dc07ef58ed743
AbooSalh <a.m0507979@gmail.com>
2023-09-01 11:18:31 +0300

    A

---
+++ Python/1-10.py
@@ -0,0 +1,22 @@
+# 1 
+# my file
+
+#2
+name = "ahmed"
+age = "18"
+country = "Egypt"
+
+# 3 
+name2 , age2 , country2 = "Osama", "38" , "Egypt"
+print(  "\"" + "Name: " + name2 + "\"")
+print(  "\"" + "Age: " + age2 + "\"")
+print(  "\"" + "country: " + country2 + "\"")
+
+# 4
+
+print("\"Hello, My Name Is " + name2 + "And Iam" + age2 + "Years Old and I Live in "+ country2 + ".\"")
+
+# 5
+print(type("me"))
+print(type("name"))
+print(type("hi"))
