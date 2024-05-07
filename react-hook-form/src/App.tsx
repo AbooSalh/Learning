b8e68d04c53876f8b4359e8d1be50e791d1bb8dd
AbooSalh <a.m0507979@gmail.com>
2024-05-07 09:32:54 +0300

    RQ

---
+++ react-hook-form/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import YTForm from './components/YTForm'
+
+function App() {
+  console.log("?");
+  
+  return (
+    <>
+    <div>
+      <YTForm/>
+    </div>
+    </>
+  )
+}
+
+export default App
