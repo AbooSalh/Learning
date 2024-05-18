105961a761e638764f32f788a8fac0d7b604e9c7
AbooSalh <a.m0507979@gmail.com>
2024-05-18 19:00:39 +0300

    1-11

---
+++ Typescript/Assginments/1-11/4/index.ts
@@ -0,0 +1,1 @@
+let arr: (number | boolean[] | (string | (string | number)[])[])[] = [1 ,2 ,3 ,4 ,5 , [true , false] , ["ss",[2]]];
---
+++ Typescript/Assginments/1-11/8/index.ts
@@ -0,0 +1,27 @@
+// Write The Function Here
+function printInConsole(...values : (string | number | boolean)[]) {
+     const results = values.map(value=>`The Value Is ${value} And Type Is ${typeof value}`)
+     return results.join("\n") + "\nDone"
+}
+// Using The Function => Do Not Edit
+console.log(printInConsole(1, 2, 3, 4, 5));
+console.log(printInConsole("A", "B", "C"));
+console.log(printInConsole(true, false, false, true, true));
+
+// Output
+// The Value Is 1 And Type Is number
+// The Value Is 2 And Type Is number
+// The Value Is 3 And Type Is number
+// The Value Is 4 And Type Is number
+// The Value Is 5 And Type Is number
+// Done
+// The Value Is A And Type Is string
+// The Value Is B And Type Is string
+// The Value Is C And Type Is string
+// Done
+// The Value Is true And Type Is boolean
+// The Value Is false And Type Is boolean
+// The Value Is false And Type Is boolean
+// The Value Is true And Type Is boolean
+// The Value Is true And Type Is boolean
+// Done
