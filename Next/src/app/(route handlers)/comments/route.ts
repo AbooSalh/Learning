07082e6cd14a7c58c37673e8c79e65043cad01a1
AbooSalh <a.m0507979@gmail.com>
2024-08-07 14:30:31 +0300

    j

---
+++ Next/src/app/(route handlers)/comments/route.ts
@@ -0,0 +1,18 @@
+import { comments } from "./data";
+export async function GET() {
+  return Response.json(comments);
+}
+export async function POST(req: Request) {
+  const comment = await req.json();
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment),{
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
+}
